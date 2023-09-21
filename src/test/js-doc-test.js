3bfd3445eecd879d06242b204744219ab861019f
fixalot <fixalot@anyssign.com>
2023-09-21 22:37:05 +0900

    Update

---
+++ src/test/js-doc-test.js
@@ -0,0 +1,70 @@
+/**
+ * @file 이 편지는 영국에서 최초로 시작되어 일년에 한바퀴를 돌면서 받는 사람에게 행운을 주었고...
+ * @since 2023-09-19
+ * 
+ * @description Changes:
+ * - 2023-09-25:
+ *     - 화면 이름 변경: '출입자 상세' -> '출입/출석내역 상세'
+ *     - 관리정보, 출입/출석내역, 메모 항목 추가
+ */
+
+/**
+ * 
+ * @param {Object} a
+ * @param {object} b
+ * @param {Array} c
+ * @param {array} d
+ * @param {Object[]} e
+ * @param {object[]} f
+ * @param {Array} g
+ * @param {array} h
+ * 
+ * @author fixalot
+ * @since 2023-09-19
+ * 
+ * @description Changes:
+ * - 2023-09-25: 화면 이름 변경: '출입자 상세' -> '출입/출석내역 상세', 관리정보, 출입/출석내역, 메모 항목 추가
+ * - 2023-09-26: 123
+ */
+function abc(a, b, c, d, e, f) {
+
+}
+
+/**
+ * @param {Date} myDate The date
+ * @param {string} myString The string
+ * @param {boolean} myFlag The flag
+ * @param {string|number} myArg I dunno
+ * @returns {string|Object} return string or number
+ */
+function myFunction(myDate, myString, myFlag, myArg) {
+  // do stuff
+}
+
+/**
+ * Returns the sum of a and b
+ * @param {number} a
+ * @param {number} b
+ * @returns {Promise} Promise object represents the sum of a and b
+ */
+function sumAsync(a, b) {
+  return Promise.resolve(a + b);
+}
+
+/**
+ * @returns {null|string|*} null 혹은 string 혹은 any를 반환한다는 뜻 (사실상 쓰나마나다 🤭)
+ */
+function getAny() {
+  //... 
+}
+
+/**
+ * My cool function.
+ *
+ * @param {Object} obj - An object.
+ * @param {string} obj.prop1 - Property 1.
+ * @param {string} obj.prop2 - Property 2.
+ */
+function willDestroyU({prop1, prop2}) {
+  // Do something with prop1 and prop2
+}
